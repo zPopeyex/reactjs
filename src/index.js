7238c054a27d81e008bb3df71580e6b7823c5016
zPopeyex <stven-457@hotmail.com>
2022-08-22 00:41:57 -0500

    Add function Greeting, Sum and others states

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hola mundo !</h1>)
+function Greeting() {
+  function Add(x, y) {
+    return x + y;
+  }
+
+  //const name = "Leonardo";
+  const married = false;
+  const user = {
+    firstName: "Steven",
+    lastName: "Garzon",
+  };
+
+  return (
+    <div>
+      <h1>Welcome : {user.firstName + " " + user.lastName}</h1>
+      <hr></hr>
+      <p>State: {married ? "Married 😀" : "Not married 😇"}</p>
+      <a>
+        El valor del boleano <b>state:</b> {married.toString()}
+      </a>
+      <p>
+        La funcion <b>Add</b> devuelve: {(10, 30)}
+      </p>
+    </div>
+  );
+}
+
+root.render(<Greeting />);
